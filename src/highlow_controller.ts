bbd841a2c8297353bcebf3d06210a61c3bffd60a
Issei Naruta <naruta@cookpad.com>
2023-04-22 13:45:20 +0900

    Update

--- src/highlow_controller.ts
+++ src/highlow_controller.ts
@@ -1,6 +1,7 @@
 import * as puppeteer from 'puppeteer';
 
 const HIGHLOW_URL_BASE = 'https://highlow.com';
+const HIGHLOW_APP_URL_BASE = 'https://app.highlow.com';
 
 export class HighLowController {
   _browser?: puppeteer.Browser;
@@ -34,19 +35,32 @@ export class HighLowController {
   async goTradingHistory() {
     const url = `${HIGHLOW_URL_BASE}/my-account/trading/trade-action-history`;
 
+    this.goto(url);
+  }
+
+  async goto(url: string) {
     const page = await this.getPage();
     await page.goto(url);
+    if (page.url().match(/\/login/)) {
+      await this.login();
+    }
   }
 
   async login() {
+    const page = await this.getPage();
+    if (!page.url().match(/\/login/)) {
+      await this.goto(`${HIGHLOW_APP_URL_BASE}/login`);
+    }
+    await page.waitForSelector('input#login-username');
+    await page.type('input#login-username', 'username', { delay: 100 });
+    await page.type('input#login-password', 'password', { delay: 100 });
+    const btn = await page.$('#login-submit-button');
+    await btn?.click();
   }
 }
 
 (async () => {
   const controller = new HighLowController();
   await controller.goTradingHistory();
-  setTimeout(() => {
-    controller.close();
-  }
-    , 30000);
+  setInterval(() => { }, 10000);
 })();
