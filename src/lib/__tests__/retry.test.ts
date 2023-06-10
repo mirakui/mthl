655b908c703bd8825da71339d51acaa5b7856f06
Issei Naruta <naruta@cookpad.com>
2023-06-10 14:36:57 +0900

    Add Retry

---
+++ src/lib/__tests__/retry.test.ts
@@ -0,0 +1,29 @@
+import { Retry, RetryCallback, RetryCallbackMatcher } from '../retry';
+
+interface TestObject {
+  status: "OK" | "ERROR";
+  retries: number;
+}
+
+test('Retry', async () => {
+  let i = 0;
+  const callback: RetryCallback<TestObject> = () => {
+    i++;
+    if (i < 3) {
+      return { status: "ERROR", retries: i - 1 };
+    }
+    else {
+      return { status: "OK", retries: i - 1 };
+    }
+  }
+  const matcher: RetryCallbackMatcher<TestObject> = (actual) => {
+    return actual.status === "OK";
+  }
+  const startTime = Date.now();
+  const result = await Retry.retryUntil(callback, matcher, { retries: 10, delay: 50 })
+  const duration = Date.now() - startTime;
+  expect(result.status).toBe("OK");
+  expect(result.retries).toBe(2);
+  expect(duration).toBeGreaterThanOrEqual(95);
+  expect(duration).toBeLessThanOrEqual(105);
+});
---
+++ src/lib/retry.ts
@@ -0,0 +1,47 @@
+export class RetryTimeoutError extends Error {
+}
+
+export interface RetryOptions {
+  retries?: number;
+  delay?: number;
+}
+
+export type RetryCallback<T> = () => T;
+export type RetryCallbackMatcher<T> = (actual: T) => Boolean;
+
+const DEFAULT_RETRIES = 10;
+const DEFAULT_DELAY = 1000;
+
+export class Retry {
+  static defaultRetries: number = DEFAULT_RETRIES;
+  static defaultDelay: number = DEFAULT_DELAY;
+
+  static setDefault(opts: RetryOptions) {
+    if (opts.retries) {
+      Retry.defaultRetries = opts.retries;
+    }
+    if (opts.delay) {
+      Retry.defaultDelay = opts.delay;
+    }
+  }
+
+  static async retryUntil<T>(callback: RetryCallback<T>, matcher: RetryCallbackMatcher<T>, opts?: RetryOptions) {
+    const retries = opts?.retries || Retry.defaultRetries;
+    const delay = opts?.delay || Retry.defaultDelay;
+    let lastResult: T | undefined;
+    for (let i = 0; i < retries; i++) {
+      lastResult = await callback();
+      if (matcher(lastResult)) {
+        return lastResult;
+      }
+      else {
+        await this.delay(delay);
+      }
+    }
+    throw new RetryTimeoutError(`Retry timeout after ${retries} retries. Last result: ${lastResult}`);
+  }
+
+  static async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
