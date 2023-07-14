e94a8c101a5318b16b6aa5b9a18bf8db9b07a2a5
Issei Naruta <naruta@cookpad.com>
2023-07-14 23:51:33 +0900

    Update

--- python/lib/server/named_pipe_server.py
+++ python/lib/server/named_pipe_server.py
@@ -8,7 +8,7 @@ class NamedPipeServer(Server):
         super().__init__(address)
         self.pipe_handle = win32pipe.CreateNamedPipe(
             self.address,
-            win32pipe.PIPE_ACCESS_DUPLEX,
+            win32pipe.PIPE_ACCESS_INBOUND,
             win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_WAIT,
             1, 65536, 65536,
             0,
@@ -16,16 +16,35 @@ class NamedPipeServer(Server):
 
     def listen(self):
         win32pipe.ConnectNamedPipe(self.pipe_handle, None)
+        pass
 
     def readlines(self):
         data = ""
         while True:
-            _, chunk = win32file.ReadFile(self.pipe_handle, 1)
-            chunk = chunk.decode()
-            if chunk == "":
-                break
-            data += chunk
-        return data.strip()
+            try:
+                result, chunk = win32file.ReadFile(self.pipe_handle, 1)
+                if result != 0:  # ReadFileが0以外の結果を返した場合
+                    raise Exception("Pipe connection error")
+                chunk = chunk.decode()
+                if chunk:  # 読み取ったデータがあれば追加する
+                    data += chunk
+                else:  # データがなければクライアントからの接続が閉じられていると判断
+                    break
+            except pywintypes.error as e:  # ReadFileが例外を投げた場合
+                if e.winerror == 109:  # ERROR_BROKEN_PIPE
+                    win32pipe.DisconnectNamedPipe(self.pipe_handle)
+                    print("Client disconnected. (109)")
+                    if data == "":
+                        win32pipe.ConnectNamedPipe(self.pipe_handle, None)
+                        print("New connection established. (109)")
+                    else:
+                        break
+                if e.winerror == 233:
+                    win32pipe.ConnectNamedPipe(self.pipe_handle, None)
+                    print("New connection established. (233)")
+                else:
+                    raise e
+        return data
 
     def close(self):
         win32file.CloseHandle(self.pipe_handle)
