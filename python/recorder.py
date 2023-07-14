a0a21908aa090cf9f65ef7c3ab08dbc2dceac7d4
Issei Naruta <naruta@cookpad.com>
2023-07-15 00:54:01 +0900

    Create recorder.py

---
+++ python/recorder.py
@@ -0,0 +1,21 @@
+from lib.server.server_factory import ServerFactory
+
+PIPE_PATH = "\\\\.\\pipe\\candlecat"
+OUT_PATH = "candlecat.txt"
+
+print(f"Opening pipe: {PIPE_PATH}")
+print(f"Out file: {OUT_PATH}")
+server = ServerFactory.create_server(PIPE_PATH)
+server.listen()
+outfile = open(OUT_PATH, "w")
+try:
+    print("Receiving data from client...")
+    while True:
+        line = server.readlines()
+        outfile.write(line + "\n")
+        print(".", end="")
+
+finally:
+    print("Closing server")
+    outfile.close()
+    server.close()
