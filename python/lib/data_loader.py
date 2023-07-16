Fix scaling bug

python/lib/data_loader.py
@@ -1,18 +1,14 @@
-import pandas as pd
-import numpy as np
 import re
+
+import numpy as np
+import pandas as pd
+
+from .constants import *
 
 
 class DataLoader:
-    def __init__(self, data_path, window_size=60, trade_duration=5):
+    def __init__(self, data_path):
         self.data_path = data_path
-        self.window_size = window_size
-        self.trade_duration = trade_duration
-
-    def load_and_preprocess(self):
-        df = self.load()
-        df = self.preprocess(df)
-        return df
 
     def load(self):
         parser = ParserFactory(self.data_path).create()
@@ -22,23 +18,29 @@ class DataLoader:
         # Calculate the trend of last WINDOW_SIZE minutes and add it to the dataset
         trends = []
         for i in range(len(df)):
-            if i < self.window_size:
+            if i < WINDOW_SIZE:
                 trends.append(0)
             else:
                 highs = np.sum(
-                    df["High"].iloc[i - self.window_size : i]
-                    > df["Open"].iloc[i - self.window_size : i]
+                    df["High"].iloc[i - WINDOW_SIZE : i]
+                    > df["Open"].iloc[i - WINDOW_SIZE : i]
                 )
                 lows = np.sum(
-                    df["Low"].iloc[i - self.window_size : i]
-                    < df["Open"].iloc[i - self.window_size : i]
+                    df["Low"].iloc[i - WINDOW_SIZE : i]
+                    < df["Open"].iloc[i - WINDOW_SIZE : i]
                 )
                 if highs > lows:
                     trends.append(1)
                 else:
                     trends.append(0)
         df["Trend"] = trends
-        return df
+
+        scaled_df = df[["Open", "High", "Low", "Close", "Trend"]].copy()
+        scaled_df[["Open", "High", "Low", "Close"]] = (
+            df[["Open", "High", "Low", "Close"]] - PRICE_MIN
+        ) / (PRICE_MAX - PRICE_MIN)
+
+        return (df, scaled_df)
 
 
 class ParserFactory:

python/lib/predictor.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from lib.constants import *
 
 
 class PredictorResult:
@@ -9,10 +10,9 @@ class PredictorResult:
 
 
 class Predictor:
-    def __init__(self, model_path, window_size=60):
+    def __init__(self, model_path):
         self.model_path = model_path
         self.buffer = np.empty((0, 6), dtype=object)
-        self.window_size = window_size
         self.model = None
 
     def load_model(self):
@@ -36,7 +36,7 @@ class Predictor:
         )
 
         # バッファが60件以上ならば、最初の行を削除
-        if len(self.buffer) > self.window_size:
+        if len(self.buffer) > WINDOW_SIZE:
             self.buffer = np.delete(self.buffer, 0, 0)
 
         return self.buffer
@@ -52,11 +52,11 @@ class Predictor:
         # We are using sigmoid in the output layer, so the prediction will be a value between 0 and 1.
         # We will interpret this as a binary prediction, with values > 0.5 corresponding to 1 (UP) and otherwise 0 (DOWN)
         # return 'HIGH' if prediction > 0.5 else 'LOW'
-        prediction = self.model.predict(expanded_ohlo, verbose=0)[0][0]
+        prediction = self.model.predict(expanded_ohlo, verbose=0)
 
         last_close_price = float(self.buffer[-1, 4])
 
-        return PredictorResult(prediction, last_close_price)
+        return PredictorResult(prediction[0][0], last_close_price)
 
     def is_ready(self):
-        return len(self.buffer) >= self.window_size
+        return len(self.buffer) >= WINDOW_SIZE

python/train.py
@@ -7,9 +7,9 @@ from lib.data_loader import DataLoader
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from lib.constants import *
 
 EPOCHS = 10
-WINDOW_SIZE = 60
 
 # Load dataset
 if len(sys.argv) < 2:
@@ -19,17 +19,16 @@ data_path = sys.argv[1]
 print(f"Loading dataset from {data_path}")
 
 data_loader = DataLoader(data_path)
-df = data_loader.load_and_preprocess()
+df = data_loader.load()
 
 print("Preparing dataset...")
+df, scaled_df = data_loader.preprocess(df)
 
 # Prepare dataset for model training
-scaler = MinMaxScaler()
-scaled_df = scaler.fit_transform(df[["Open", "High", "Low", "Close", "Trend"]])
 x, y = [], []
-for i in range(WINDOW_SIZE, len(scaled_df)):
+for i in range(WINDOW_SIZE, len(scaled_df) - TRADE_DURATION):
     x.append(scaled_df[i - WINDOW_SIZE : i])
-    y.append(1 if df["Open"].iloc[i] > df["Close"].iloc[i - 1] else 0)
+    y.append(1 if df["Close"].iloc[i] < df["Open"].iloc[i + TRADE_DURATION] else 0)
 x = np.array(x)
 y = np.array(y)
 
@@ -73,7 +72,7 @@ print(f"Loading model: {model_name}")
 # .keras だと compile=True (デフォルト) でエラーになる。 .h5 だとならない
 loaded_model = tf.keras.saving.load_model(model_name, compile=False)
 print(f"Successfully loaded model: {model_name}")
-y_pred2 = model.predict(x_test)
+y_pred2 = loaded_model.predict(x_test)
 y_pred2 = np.where(y_pred2 > 0.5, 1, 0)
 
 # Evaluate the model